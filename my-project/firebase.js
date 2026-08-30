7cd0cae668ec7967ff44ad0ed08e4d534082b4c7
shiven jindal <shivenjindal3@gmail.com>
2026-08-30 12:08:08 +0530

    config firebase
    LiveReview Pre-Commit Check: skipped (iter:1, coverage:0%)

---
+++ my-project/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "http://sih-backend-3c981.firebaseapp.com",
+  projectId: "sih-backend-3c981",
+  storageBucket: "sih-backend-3c981.appspot.com",
+  messagingSenderId: "845678901234",
+  appId: "1:845678901234:web:abcdef1234567890"
+};
